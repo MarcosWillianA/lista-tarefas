ec112e8c774685c18639246b96b09296d3e559de
Marcos Willian <166168504+MarcosWillianA@users.noreply.github.com>
2024-10-09 14:25:51 -0300

    Adicionar tarefas
    A lógica de adicionar tarefas foi criada

--- script/app.js
+++ script/app.js
@@ -1,2 +1,20 @@
 const escreverTarefa = document.querySelector('#escreverTarefa');
 const adicionarTarefa = document.querySelector('#adicionarTarefa');
+const listaTarefas = document.querySelector('#listaTarefas');
+
+
+
+adicionarTarefa.addEventListener('click', () => {
+    if (escreverTarefa.value === '') {
+        console.log('Tá vazio');
+        alert ('Você precisa escrever alguma coisa')
+    }
+    else {
+        let tarefaEscrita = escreverTarefa.value;
+        console.log(tarefaEscrita);
+        let li = document.createElement('li');
+        li.innerHTML = tarefaEscrita;
+        listaTarefas.appendChild(li);
+    };
+});
+
