ad1c20bf405dde4896e761cb38b0677d7cb59ffd
Marcos Willian <166168504+MarcosWillianA@users.noreply.github.com>
2024-10-09 16:25:31 -0300

    Pronto
    Eu acho

--- script/app.js
+++ script/app.js
@@ -2,18 +2,14 @@ const escreverTarefa = document.querySelector('#escreverTarefa');
 const adicionarTarefa = document.querySelector('#adicionarTarefa');
 const listaTarefas = document.querySelector('#listaTarefas');
 
-// Função para carregar as tarefas do localStorage
-function carregarTarefas() {
-    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
-    tarefas.forEach(tarefa => adicionarTarefaNaLista(tarefa));
-}
-
 adicionarTarefa.addEventListener('click', () => {
     if (escreverTarefa.value === '') {
-        alert ('Você precisa escrever alguma coisa')
+        alert ('Você precisa escrever alguma coisa');
+        salvarDados();
     }
     else {
         let tarefaEscrita = escreverTarefa.value;
+        console.log(tarefaEscrita);
         let li = document.createElement('li');
         let p = document.createElement('p');
         let remover = document.createElement('button');
@@ -32,14 +28,23 @@ adicionarTarefa.addEventListener('click', () => {
         p.addEventListener('click', () => {
             console.log('clicou');
             p.classList.add('marcado');
-            
         });
 
         remover.addEventListener('click', () => {
-            li.remove();
-            
+            li.remove();    
+            salvarDados();        
         })
+        salvarDados();
     };
-    
+    salvarDados();
 });
 
+function salvarDados() {
+    localStorage.setItem('data', listaTarefas.innerHTML);
+}
+
+function mostrarTarefas() {
+    listaTarefas.innerHTML = localStorage.getItem('data');
+}
+
+mostrarTarefas();
